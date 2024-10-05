72a4dacbe3907715cae29249699e8879d1993ee2
admin <admin@admins-MBP.home>
2024-10-05 16:51:21 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavBar from "./component/Structural Component/NavBar";
 import Main from "./component/Structural Component/Main";
-import tempMovieData from "../src/component/tempMovieData";
+
 import { useEffect, useState } from "react";
 import SearchBar from "./component/Presentation /SearchBar";
 import NumResult from "./component/Presentation /NumResult";
@@ -8,7 +8,7 @@ import Box from "./component/State/Box";
 import ListOfMovies from "./component/State/ListOfMovies";
 import WatchSummary from "./component/Presentation /WatchSummary";
 import WatchList from "./component/Presentation /WatchList";
-import tempWatchedData from "./component/temWatchedData";
+
 import Loader from "./component/Presentation /Loader";
 import ErrorMessage from "./component/Presentation /ErrorMessage";
 import MovieDetails from "./component/Presentation /MovieDetails";
@@ -17,33 +17,16 @@ const KEY = "dc85bd43";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
-  const tempQuery = "Blade";
 
-  /*
-
-  useEffect(function () {
-    console.log("After initial render");
-  }, []);
-
-  useEffect(function () {
-    console.log("After every render");
+  //const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
   });
-
-  useEffect(
-    function () {
-      console.log("D");
-    },
-    [query]
-  );
-
-  console.log("During render");
-
-  */
-
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
@@ -54,11 +37,20 @@ export default function App() {
 
   function handleAddWatch(movie) {
     setWatched((watched) => [...watched, movie]);
+
+    //localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
--- src/component/Presentation /SearchBar.js
+++ src/component/Presentation /SearchBar.js
@@ -1,4 +1,30 @@
+import { useEffect, useRef } from "react";
+
 const SearchBar = ({ query, setQuery }) => {
+  //useEffect(function () {
+  //const el = document.querySelector(".search");
+  //console.log(el);
+  //el.focus();
+  //}, []);
+
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <div>
       <input
@@ -7,6 +33,7 @@ const SearchBar = ({ query, setQuery }) => {
         placeholder="Search movies..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
       />
     </div>
   );
